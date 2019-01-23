2ac6e78e3b7f860463c8b49c376b24dba8e58db7
rising30 <ekozinets@inbox.ru>
2019-01-24 01:26:21 +0300

    complete ht2

--- HW2.c
+++ HW2.c
@@ -11,27 +11,50 @@ void recursion (int a)
 	printf("%d", a % 2);	
 }
 
-void degree (int a, int b, int c)
+void degree (int a, int b, int q)
 {
 	if (b>1)
 	{
-		degree (a*c, b-1, c);
+		degree (a*q, b-1, q);
 	}
-	printf("%d \n", a);
+	printf("\n%d", a);
 	
+}
+
+int fastdegree (int base, int sign , int c)
+{
+	if (sign%2==0 && sign>=2)
+	{
+		base*=base;
+		sign/=2;
+		fastdegree (base, sign, c);
+	}
+	else if (sign>1)
+	{
+		c*=base;
+		sign--;
+		fastdegree (base, sign, c);	
+	}
+	base*=c;
+	printf("\n%d ", base);
+	
+	return 0;
 }
 
 
 int main ()
 {
-	int a; 
-	int b;
+	int a; 	//число
+	int b;	//степень
+	int q;
 	int c;
 	scanf("%d", &a);
 	scanf("%d", &b);
-	c=a;
-	//recursion(a);
-	degree(a,b,c);
+	q=a;	//запоминаем число для рекурсивного ввода в степень
+	c=1;	//запоминаем result для рекурсивного ввода в степень со свойством четности
+	recursion(a);
+	degree(a, b, q);
+	fastdegree (a, b, c);
 		
 	return 0;
 }
